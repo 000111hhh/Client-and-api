fc6f883c6037757a931eebd5008604cd9cc5c39d
000111hhh <142199996+000111hhh@users.noreply.github.com>
2024-05-15 21:50:59 +0800

    Update chatserver.js
    删除隐私代码

--- chatserver.js
+++ chatserver.js
@@ -55,46 +55,10 @@ function sendMessage(id) {
   chatInput.value = "";
 }
 
-// async function getMessage(message) {
-//   try {
-//     const response = await fetch("https://api.binjie.fun/api/generateStream", {
-//       method: 'POST',
-//       headers: {
-//         "Content-Type": "application/json",
-//         "origin": "https://chat18.aichatos.xyz"
-//       },
-//       body: JSON.stringify({
-//         "prompt": "你好",
-//         "stream": false,
-//         "system": "",
-//         "network": false,
-//         "userId": "#/chat/1700836720616",
-//         "withoutContext": false
-//       })
-//     });
-
-//     if (response.ok) {
-//       const textData = await response.text();
-//       console.log(textData);
-//       ChatReply(textData); 
-//     } else {
-//       throw new Error('网络请求失败');
-//     }
-//   } catch (error) {
-//     console.log('捕获到错误:', error);
-//   }
-// }
-
-
 async function getMessage(message,id) {
 
     const data = {
-    prompt: `${message}`,
-    stream: false,
-    system: "",
-    network: false,
-    userId: `#/chat/${id}`,
-    withoutContext: false
+    "这里是请求参数"
   };
 
   await fetch("http://localhost:3000/api/sendMessage", 
